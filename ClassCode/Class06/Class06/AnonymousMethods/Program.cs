10df270b4d2da4fd20ad39901f0ee409b9a76141
MartinLutovski <martinlutovski@gmail.com>
2025-05-13 20:49:47 +0200

    Class06
    Linq and lambda

---
+++ ClassCode/Class06/Class06/AnonymousMethods/Program.cs
@@ -0,0 +1,120 @@
+﻿int Sum (int x, int y) {
+    return x + y;
+}
+
+
+int Diff(int x, int y)
+{
+    return x - y;
+}
+
+
+int Multiply(int x, int y)
+{
+    return x * y;
+}
+
+List<string> names = new() {
+    "Martin",
+    "Slave",
+    "Ana",
+    "Sandra",
+    "Angel"
+};
+
+Console.WriteLine("==================== FUNC =====================");
+#region Func
+// Func is a special type of delegate in C#
+// and it ALWAYS returns a value
+
+Func<int, int, int> sumOfTwo = (x, y) => x + y;
+
+Func<int, int, int> diffOfTwo = (x, y) => x - y;
+
+Func<int, int, int> mulOfTwo = (x, y) => x * y;
+
+Func<int, int, bool> checkIfLarger = (x, y) =>
+{
+    if (x > y)
+    {
+        return true;
+    }     
+        return false;
+    
+
+};
+
+Console.WriteLine($"The sum of 5 and 10 is:{sumOfTwo(5, 10)}");
+Console.WriteLine($"The difference of 10 and 4 is:{diffOfTwo(10, 4)}");
+Console.WriteLine($"The product of 123 and 10 is:{mulOfTwo(123, 10)}");
+if (checkIfLarger(10, 20))
+{
+    Console.WriteLine("10 is larger.");
+}
+else
+{
+    Console.WriteLine("20 is larger");
+}
+
+
+
+Func<List<string>, bool> isListEmpty = (list) => list.Count == 0;
+
+Console.WriteLine($"Is the list of  empty: {isListEmpty(names)}");
+
+
+#endregion
+
+Console.WriteLine("==================== ACTION =====================");
+
+#region Action
+// Action is a special type of delegate in C#
+// which can accept input parameters but DOESN'T return anything
+// It always returns void
+
+
+
+// Example of Action that has no input parameter
+Action hello = () => Console.WriteLine("Hello there!");
+hello();
+
+
+// Example of Action that has one input parameters
+Action<string> printInRed = (text) =>
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(text);
+    Console.ResetColor();
+};
+
+printInRed("This will be printed in red text.");
+
+
+// Example of Action that has two input parameters
+Action<string, ConsoleColor> printInColor = (text, color) =>
+{
+    Console.ForegroundColor = color;
+    Console.WriteLine(text);
+    Console.ResetColor();
+};
+
+printInColor("This will be printed in green text.", ConsoleColor.Green);
+
+#endregion
+
+
+Console.WriteLine("==================== Higher Order Functions use =====================");
+
+#region Higher Order Functions use
+
+string foundMartin = names.Find(x => x == "Martin");
+Console.WriteLine($"Found: {foundMartin}");
+
+Predicate<string> isAngel = x => x == "Angel";
+
+string foundAngel = names.Find(isAngel);
+
+Console.WriteLine($"Found: {foundAngel}");
+
+
+#endregion
---
+++ ClassCode/Class06/Class06/Qinshift.Class06/Qinshift.LinqMethods/Program.cs
@@ -0,0 +1,113 @@
+﻿// WHERE ()
+
+
+// SQL Like syntax
+
+using Qinshift.Class06.LinqMethods;
+using Qinshift.Class06.LinqMethods.Entities;
+using Qinshift.Class06.LinqMethods.Helpers;
+
+Console.WriteLine("===== WHERE =====");
+// SQL like syntax
+
+IEnumerable<Student> findBobSql = from std in Database.Students
+                                  where std.FirstName == "Bob"
+                                  select std;
+
+// Lambda LINQ syntax
+IEnumerable<Student> findBobLambda = Database.Students
+    .Where(x => x.FirstName == "Bob");
+
+findBobSql.ToList().PrintEntities();
+findBobLambda.ToList().PrintEntities();
+
+
+// SELECT
+
+Console.WriteLine("===== SELECT =====");
+
+// SQL like syntax
+
+List<string> firstNamesSql = (from std in Database.Students
+                             select std.FirstName).ToList();
+
+List<string> studentFullNamesSql = (from std in Database.Students
+                                    select $"{std.FirstName} {std.LastName}").ToList();
+
+firstNamesSql.PrintSimple();
+studentFullNamesSql.PrintSimple();
+
+// Lambda LINQ syntax
+
+List<string> firstNamesLINQ = Database.Students
+    .Select(x => x.FirstName)
+    .ToList();
+
+List<string> studentFullNamesLINQ = Database.Students.Select(x => $"{x.FirstName} {x.LastName}").ToList();
+
+firstNamesLINQ.PrintSimple();
+studentFullNamesLINQ.PrintSimple();
+
+
+Console.WriteLine("===== COMPLEX QUERIES =====");
+
+// SQL like syntax
+List<Student> partTimeProgrammingStudentsSql = (from std in Database.Students
+                                  where std.IsPartTime && std.Subjects is not null == true
+                                  where (from sub in std.Subjects
+                                         where sub.Type == Academy.Programming
+                                         select sub).ToList().Count !=0 select std).ToList();
+
+                                 
+// Print the results
+Console.WriteLine("Part-time students enrolled in programming subjects:");
+partTimeProgrammingStudentsSql.ToList().PrintEntities();
+
+// Lambda LINQ syntax
+
+List<Student> partTimeProgrammingStudentsLinq =
+    Database.Students.Where(x => x.IsPartTime && x.Subjects is not null)
+    .Where(x => x.Subjects.Where(y => y.Type == Academy.Programming).ToList().Count != 0).ToList();
+
+
+partTimeProgrammingStudentsLinq.PrintEntities();
+
+
+// FIRST / SINGLE / LAST / ORDEFAULT
+Console.WriteLine("===== FIRST / SINGLE / LAST / ORDEFAULT =====");
+
+List<string> emptyListOfStrings = new();
+List<Student> emptyListOfStudents = new();
+
+Console.WriteLine(Database.Students.First().Info());
+Console.WriteLine(Database.Students.First(x => x.FirstName.StartsWith("T")).Info());
+
+Console.WriteLine(emptyListOfStudents.FirstOrDefault());
+
+Console.WriteLine(Database.Students.SingleOrDefault(x => x.Id == 123)?.Info());
+
+// "?" null conditional operator
+
+// SELECT MANY
+
+Console.WriteLine("===== SELECT MANY =====");
+
+var partTimeStudentsallSubjects = Database.Students
+        .Where(x => x.IsPartTime && x.Subjects is not null)
+        .SelectMany(x => x.Subjects).ToList();
+
+partTimeStudentsallSubjects.PrintEntities();
+
+
+List<Subject> partTimeStudentsAllSubjectsDistinct = Database.Students
+        .Where(x => x.IsPartTime && x.Subjects is not null)
+        .SelectMany(x => x.Subjects).Distinct().ToList();
+
+
+partTimeStudentsAllSubjectsDistinct.PrintEntities();
+
+var exceptPartTime = Database.Students.Except(Database.Students.Where(x => x.IsPartTime)).ToList();
+
+var exceptWithWhere = Database.Students.Where(x => x.IsPartTime).ToList();
+
+exceptPartTime.PrintEntities();
