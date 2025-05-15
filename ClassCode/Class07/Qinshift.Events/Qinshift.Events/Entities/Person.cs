8ae640316927ad5accf3671fb041bc329e6b554f
MartinLutovski <martinlutovski@gmail.com>
2025-05-15 19:56:04 +0200

    Class07
    reup

---
+++ ClassCode/Class07/Qinshift.Events/Qinshift.Events/Entities/Person.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace Qinshift.Events.Entities
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public bool HasWokenUp { get; set; } = false;
+
+        public void WakeUp()
+        {
+            Console.WriteLine($"{Name} waaaakeeeeeeeeeeee up.");
+            HasWokenUp = true;
+        }
+    }
+        
+   }
--- ClassCode/Class07/Qinshift.Events/Qinshift.Events/Program.cs
+++ ClassCode/Class07/Qinshift.Events/Qinshift.Events/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Qinshift.Events.Entities;
+
+
+AlarmClock alarmClock = new AlarmClock();
+Person person = new Person() { Name = "Martin" };
+Person ana = new Person() { Name = "Ana" };
+Person sandra = new Person() { Name = "Sandra" };
+
+alarmClock.OnAlarmSounded += person.WakeUp;
+
+alarmClock.OnAlarmSounded += ana.WakeUp;
+
+alarmClock.OnAlarmSounded += sandra.WakeUp;
+
+alarmClock.Start();
+
+Thread.Sleep(5000);
+
+if (person.HasWokenUp)
+{
+    Console.WriteLine($"{person.Name} is awake.");
+    alarmClock.OnAlarmSounded -= person.WakeUp;
+}
