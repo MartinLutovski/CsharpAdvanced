6356ae29a059bd398f8b19a5e53bc7aad368ef66
MartinLutovski <martinlutovski@gmail.com>
2025-05-20 20:42:49 +0200

    workshop02

---
+++ ClassCode/WorkshopClass08/Workshop_Part2/TaxiManager9000/TaxiManager9000.Services/IDb.cs
@@ -0,0 +1,11 @@
+﻿using TaxiManager9000.Domain.Models;
+
+namespace TaxiManager9000.Services
+{
+    internal interface IDb<T> where T : BaseEntity
+    {
+        object GetAll();
+        User GetById(int id);
+        void Update(User user);
+    }
+}
