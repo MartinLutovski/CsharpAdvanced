d0adf2eb6e1b1f14a69779b9c07878d6038527c5
MartinLutovski <martinlutovski@gmail.com>
2025-05-23 20:43:55 +0200

    Class10
    Workshop TaxiManager9000

---
+++ ClassCode/WorkshopClass10/Workshop_Part3/TaxiManager9000/TaxiManager9000.Services/Interfaces/IUIService.cs
@@ -0,0 +1,20 @@
+﻿using TaxiManager9000.Domain.Enums;
+using TaxiManager9000.Domain.Models;
+using TaxiManager9000.Services.Enums;
+
+namespace TaxiManager9000.Services.Interfaces
+{
+    public interface IUIService
+    {
+        List<MenuChoice> MenuItems { get; set; }
+        UserService LogInMenu();
+
+        void Welcome(User user);
+
+        int MainMenu(Role role);
+
+        int ChoseMenu<T>(List<T> items);
+
+        int ChoseEntitiesMeny<T>(List<T> entities) where T : BaseEntity;
+    }
+}
---
+++ ClassCode/WorkshopClass10/Workshop_Part3/TaxiManager9000/TaxiManager9000.Services/UIService.cs
@@ -0,0 +1,127 @@
+﻿using TaxiManager9000.Domain.Enums;
+using TaxiManager9000.Domain.Models;
+using TaxiManager9000.Services.Enums;
+using TaxiManager9000.Services.Interfaces;
+using TaxiManager9000.Services.Utilities;
+
+namespace TaxiManager9000.Services
+{
+    public class UIService : IUIService
+    {
+        private List<MenuChoice> _menuItems;
+        public List<MenuChoice> MenuItems
+        {
+            get => _menuItems;
+            set
+            {
+                if (_menuItems != null)
+                {
+                    _menuItems.Clear();
+                }
+                _menuItems = value;
+            }
+        }
+
+        public User LogInMenu()
+        {
+            Console.WriteLine("TaxiManager9000!");
+            ExtendedConsole.Separator();
+            Console.WriteLine("Log in:");
+            string username = ExtendedConsole.GetInput("Username: ");
+            string password = ExtendedConsole.GetInput("Password: ");
+
+            return new User { Username = username, Password = password };
+        }
+
+        public void Welcome(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int MainMenu(Role role)
+        {
+            var menuItems = new List<MenuChoice>() { MenuChoice.ChangePassword, MenuChoice.Exit };
+            switch (role)
+            {
+                case Role.Admin:
+                    menuItems = MenuItems.Prepend(MenuChoice.AddNewUser).ToList();
+                    menuItems = menuItems.Prepend(MenuChoice.RemoveExistingUser).ToList();
+                    break;
+                case Role.Manager:
+                    menuItems = menuItems.Prepend(MenuChoice.DriverManager).ToList();
+                    menuItems = menuItems.Prepend(MenuChoice.TaxiLicenseStatus).ToList();
+                    menuItems = menuItems.Prepend(MenuChoice.DriverManager).ToList();
+                    break;
+                case Role.Maintenance:
+                    menuItems = menuItems.Prepend(MenuChoice.ListAllCars).ToList();
+                    menuItems = menuItems.Prepend(MenuChoice.LicensePlateStatus).ToList();
+                    break;
+
+            }
+            MenuItems = menuItems;
+            return ChoseMenu(MenuItems);
+        }
+
+        public int ChoseMenu<T>(List<T> items)
+        {
+            Console.Clear();
+            if(items.Count == 0)
+            {
+                ExtendedConsole.NoItemsMessage<T>();
+                Console.ReadLine();
+                return -1;
+            }
+            while (true)
+            {
+                Console.WriteLine("Enter a number to choose one of the following:");
+                for (int i = 0; i < items.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}: {items[i]}");
+                }
+                int choice = StringValidator.ValidateNumber(Console.ReadLine(), items.Count);
+                if (choice == -1)
+                {
+                    ExtendedConsole.WriteLine($"[Error] Input incorrect! Please try again!", ConsoleColor.Red);
+                    Console.ReadLine();
+                    Console.Clear();
+                    continue;
+                }
+                return choice;
+            }
+        }
+
+        public int ChoseEntitiesMeny<T>(List<T> entities) where T : BaseEntity
+        {
+            Console.Clear();
+            if (entities.Count == 0)
+            {
+                ExtendedConsole.NoItemsMessage<T>();
+                Console.ReadLine();
+                return -1;
+            }
+            while (true)
+            {
+                Console.WriteLine("Enter a number to choose one of the following:");
+                for (int i = 0; i < entities.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}: {entities[i].Print()}");
+                }
+                int choice = StringValidator.ValidateNumber(Console.ReadLine(), entities.Count);
+                if (choice == -1)
+                {
+                    ExtendedConsole.WriteLine($"[Error] Input incorrect! Please try again!", ConsoleColor.Red);
+                    Console.ReadLine();
+                    Console.Clear();
+                    continue;
+                }
+                return choice;
+            }
+        }
+
+        UserService IUIService.LogInMenu()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    }
+
