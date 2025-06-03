7288d7da3113b6ac0ce4e8da1085b3d62e5565a4
MartinLutovski <martinlutovski@gmail.com>
2025-06-03 20:30:39 +0200

    Class13
    Asynchronous execution, Threads,Tasks, Async/Await🔹

---
+++ ClassCode/Class13/Qinshift.AsyncAwait/Qinshift.Class13/Program.cs
@@ -0,0 +1,49 @@
+﻿void sendMessage(string message)
+{
+    Console.WriteLine("Sending message ...");
+    Thread.Sleep(7000);
+    Console.WriteLine($"The message '{message}' was sent!");
+}
+
+
+async Task SendMessageAsync(string message)
+{
+    Console.WriteLine("Sending message ...");
+   await Task.Run(() =>
+    {
+        Thread.Sleep(7000);
+        Console.WriteLine($"The message '{message}' was sent!");
+
+        });
+    Console.WriteLine($"The message '{message}' was sent!");
+}
+
+
+void ShowAd(string product)
+{
+    Console.WriteLine("While you wait let us show you an ad!");
+    Thread.Sleep(1000);
+    Console.WriteLine("Buy the best product in the world");
+    Thread.Sleep(1000);
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.Write($"{product}");
+    Thread.Sleep(1000);
+    Console.ResetColor();
+    Console.Write(" now and get");
+    Thread.Sleep(1000);
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write(" FREE");
+    Console.ResetColor();
+    Thread.Sleep(1000);
+    Console.WriteLine(" shipping worldwide!");
+    Console.ReadLine();
+}
+
+sendMessage("Hello there!");
+
+var x = SendMessageAsync("Hello there!");
+Console.WriteLine(x.Status);
+
+ShowAd("Tomato");
+Console.ReadLine();
+Console.WriteLine(x.Status);
---
+++ ClassCode/Class13/Qinshift.Task/Qinshift.Class13/Program.cs
@@ -0,0 +1,69 @@
+﻿Console.WriteLine("App start ...");
+
+// Single Task example
+
+Task myTask = new Task(() =>
+{
+    Thread.Sleep(2000);
+    Console.WriteLine("This comes into the console after 2 seconds.");
+
+});
+
+myTask.Start();
+
+// Single Task with return type
+
+Task<int> valueTask = new Task<int>(() =>
+{
+    Thread.Sleep(2000);
+    Console.WriteLine("We can now get the number ...");
+
+    throw new Exception("Something went wrong!");
+});
+Console.WriteLine("Task status:" + valueTask.Status);
+valueTask.Start();
+Console.WriteLine("Task status:" + valueTask.Status);
+
+Thread.Sleep(3000);
+if(valueTask.IsCompleted)
+{
+    Console.WriteLine("Task is completed!");
+}
+else
+{
+    Console.WriteLine("Task is still running on some thread!");
+}
+
+if (valueTask.IsCompletedSuccessfully)
+{
+    Console.WriteLine("My value task is complete succesfully!");
+}
+else
+{
+    Console.WriteLine("My value task is not complete succesfully!");
+}
+
+    Task.Run(() =>
+    {
+        Thread.Sleep(3000);
+        Console.WriteLine("This is executed immediately!");
+    });
+
+Console.WriteLine(valueTask.Result);
+
+Console.WriteLine("App end ...");
+
+//for (int i = 0; i <= 20; i++)
+//{
+//    int temp = i;
+//    Task.Run(() =>
+//    {
+//        Thread.Sleep(2000);
+//        Console.WriteLine("Task number:" + temp);
+//    });
+//}
+
+
+
+Console.ReadLine(); // This line will be executed immediately after starting the task, not waiting for it to complete.
+// Because the task is running asynchronously, the program will not wait for it to finish before moving on to the next line of code.
